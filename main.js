7a6b42d21816d3e95f993a4a020ff76fd52af567
AmirM Afzal <104265017+AmirmAfzal@users.noreply.github.com>
2025-05-14 08:08:31 -0700

    index11

---
+++ main.js
@@ -0,0 +1,101 @@
+// console.log("Test")
+// var x = 2
+// var x = "text"
+// var x = []
+// var x = {}
+// var x = null
+// var x = undefined
+// var x = true
+// var x = false
+
+// const y = 1
+
+// let z = 1
+// console.log(z)
+// z = 2
+// console.log(z)
+
+// const y = 1
+// console.log(y)
+// y = 2
+// console.log(y)
+
+// let test = 1
+// console.log(test)
+// test = "text"
+// console.log(test)
+
+// let text = "Hello" 
+// console.log(text)
+// const x = 2
+// const y = 9
+// alert("Hi!")
+// const input = "salam"
+
+// if (input == "Hello") {
+//     console.log("Hi!")
+// } else if (input == "bye") {
+//     console.log("bye")
+// } else if (input == "help") {
+//     console.log("how can i help")
+// } else {
+//     console.log("i dont understand")
+// }
+// if (input == "Hello") {
+//     console.log("Hi!")
+// } else {
+//     if (input == "bye") {
+//         console.log("bye bye")
+//     } else {
+//         if (input == "help") {
+//             console.log("how can i help")
+//         } else {
+
+//             console.log("i dont understand")
+//         }
+//     }
+// }
+// if (x >= 2 || y == 10) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+// if (x > 1 && y == 10) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+// if (x < 5) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+
+// console.log("mutiply" ,2*2)
+// console.log(2/2)
+// console.log(2+2)
+// console.log(2-2)
+
+
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// let i = 0
+// while (i < 5) {
+//     console.log(i)
+//     i++
+// }
+
+
+// console.log("hellow")
+// alert("hi")
+function myFunc(number , number2){
+    console.log(number * number2)
+}
+
+console.log("hello" , "world")
+
