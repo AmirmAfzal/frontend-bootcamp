8e276018f886fff8eea1c50e0931b9c8d4cff787
AmirM Afzal <104265017+AmirmAfzal@users.noreply.github.com>
2025-06-07 08:07:20 -0700

    session18

---
+++ session18/index.js
@@ -0,0 +1,39 @@
+// Commonjs
+// const express = require("express")
+// ES6 Modules
+// music.youtube.com/
+// license-market.ir/product/MasterClass
+
+
+// import express from "express"
+// import { v4 as uuidv4 } from 'uuid';
+// const app = express()
+// app.get('/', (req, res) => {
+//     res.send({
+//         message : "Response from the server",
+//         data : [
+//             { id: 1, name: "Item 1" },
+//             { id: 2, name: "Item 2" },  
+//             { id: 3, name: "Item 3" },
+//             { id: 4, name: "Item 4" },
+//             { id: 5, name: "Item 5" }
+//         ]
+//     })
+// })
+// app.get('/id', (req, res) => {
+//     const id = uuidv4()
+//     res.send(`<h1>Id:</h1><p>Your unique ID is: ${id}</p>`)
+// })
+// app.listen(3000)
+
+
+const func = async()=>{
+    return 0;
+}
+
+// Async Await JavaScript 
+// Learn one npm package
+// Axios package
+console.log("Hi")
+const number = await func()
+console.log(number)
