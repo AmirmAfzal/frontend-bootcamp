aafdbe38cb97c640a0bd19515d083414237a14b9
AmirM Afzal <104265017+AmirmAfzal@users.noreply.github.com>
2025-06-12 08:20:11 -0700

    session19

---
+++ session19/main.js
@@ -0,0 +1,27 @@
+// async function name() {
+
+// }
+
+// CommonJs
+// const axios = require('axios');
+
+// ES6 Module
+import axios from "axios"
+
+console.log("Sending request to randomuser.me API...");
+axios.get("https://randomuser9.me/api/").then((res) => {
+    console.log("request completed.");
+}).catch((err) => {
+    console.error("request failed:");
+})
+
+console.log("End");
+
+const main = async () => {
+    console.log("Sending request to randomuser.me API...");
+    const res = await axios.get("https://randomuser.me/api/")
+    console.log(res.data)
+    console.log("End");
+}
+
+main()
